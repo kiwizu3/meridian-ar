378b65250aec1abe8be8bf73c951b50e109edcaf
Amaneth Kodippli <amaneth.kodippili@arimaclanka.com>
2025-07-16 19:45:22 +0530

    ( Merge Request ) Home Page video with sound

--- components/HomePageMainVideo.tsx
+++ components/HomePageMainVideo.tsx
@@ -1,96 +1,113 @@
-'use client';
+// 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-import videoThumbnail from '@/public/images/video-frame-home.png';
+// import { useEffect, useRef, useState } from 'react';
+// import videoThumbnail from '@/public/images/video-frame-home.png';
 
-const HomePageMainVideo = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isClient, setIsClient] = useState(false);
+// const HomePageMainVideo = () => {
+//   const videoRef = useRef<HTMLVideoElement>(null);
+//   const [isPlaying, setIsPlaying] = useState(false);
+//   const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    const video: any = videoRef.current;
-    const playButton = document.querySelector('.snap-motion-item.smi-play');
+//   useEffect(() => {
+//     const video: any = videoRef.current;
+//     const playButton = document.querySelector('.snap-motion-item.smi-play');
 
-    if (video && playButton) {
-      const handlePlay = () => {
-        setIsPlaying(true);
-      };
+//     if (video && playButton) {
+//       const handlePlay = () => {
+//         setIsPlaying(true);
+//       };
 
-      const handlePauseOrEnded = () => {
-        setIsPlaying(false);
-      };
+//       const handlePauseOrEnded = () => {
+//         setIsPlaying(false);
+//       };
 
-      video.addEventListener('play', handlePlay);
-      video.addEventListener('pause', handlePauseOrEnded);
-      video.addEventListener('ended', handlePauseOrEnded);
+//       video.addEventListener('play', handlePlay);
+//       video.addEventListener('pause', handlePauseOrEnded);
+//       video.addEventListener('ended', handlePauseOrEnded);
 
-      return () => {
-        video.removeEventListener('play', handlePlay);
-        video.removeEventListener('pause', handlePauseOrEnded);
-        video.removeEventListener('ended', handlePauseOrEnded);
-      };
-    }
-  }, []);
+//       return () => {
+//         video.removeEventListener('play', handlePlay);
+//         video.removeEventListener('pause', handlePauseOrEnded);
+//         video.removeEventListener('ended', handlePauseOrEnded);
+//       };
+//     }
+//   }, []);
 
-  useEffect(() => {
-    const playButton = document.querySelector('.snap-motion-item.smi-play');
-    if (playButton) {
-      if (isPlaying) {
-        playButton.classList.add('hidden');
-      } else {
-        playButton.classList.remove('hidden');
-      }
-    }
+//   useEffect(() => {
+//     const playButton = document.querySelector('.snap-motion-item.smi-play');
+//     if (playButton) {
+//       if (isPlaying) {
+//         playButton.classList.add('hidden');
+//       } else {
+//         playButton.classList.remove('hidden');
+//       }
+//     }
 
-    const handleAudioPlay = (event: Event) => {
-      const video = videoRef.current;
-      if (video && event.target !== video && isPlaying) {
-        video.pause();
-      }
-    };
+//     const handleAudioPlay = (event: Event) => {
+//       const video = videoRef.current;
+//       if (video && event.target !== video && isPlaying) {
+//         video.pause();
+//       }
+//     };
 
-    document.addEventListener('play', handleAudioPlay, true);
+//     document.addEventListener('play', handleAudioPlay, true);
 
-    return () => {
-      document.removeEventListener('play', handleAudioPlay, true);
-    };
-  }, [isPlaying]);
+//     return () => {
+//       document.removeEventListener('play', handleAudioPlay, true);
+//     };
+//   }, [isPlaying]);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
 
+//   return (
+//     <div>
+//       <video
+//         id="videoPlayer"
+//         ref={videoRef}
+//         className="max-h-[79vh] max-w-[1280px] w-full object-cover appl1-hdvd-xx rounded-2xl"
+//         controls
+//         poster={videoThumbnail?.src}
+//       >
+//         {isClient &&
+//           typeof window !== 'undefined' &&
+//           window?.innerWidth > 700 && (
+//             <>
+//               <source
+//                 src="https://firebasestorage.googleapis.com/v0/b/todo-app-b057b.appspot.com/o/LB%20Finance%20Annual%20Report%20-%202024-new.mp4?alt=media&token=c9428274-cd10-4c9c-aa73-5ff2df926449"
+//                 type="video/mp4"
+//               />
+//               <track
+//                 kind="captions"
+//                 src="https://firebasestorage.googleapis.com/v0/b/todo-app-b057b.appspot.com/o/LB%20Finance%20Annual%20Report%20-%202024-new.mp4?alt=media&token=c9428274-cd10-4c9c-aa73-5ff2df926449"
+//                 srcLang="en"
+//                 label="English captions"
+//                 default
+//               />
+//             </>
+//           )}
+//         Your browser does not support the video tag.
+//       </video>
+//     </div>
+//   );
+// };
+
+// export default HomePageMainVideo;
+
+// components/HiddenVideoPlayer.js
+
+export default function HomePageMainVideo() {
   return (
     <div>
       <video
-        id="videoPlayer"
-        ref={videoRef}
+        src="/videos/LB lion animation_v4_intro_SFX.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
         className="max-h-[79vh] max-w-[1280px] w-full object-cover appl1-hdvd-xx rounded-2xl"
-        controls
-        poster={videoThumbnail?.src}
-      >
-        {isClient &&
-          typeof window !== 'undefined' &&
-          window?.innerWidth > 700 && (
-            <>
-              <source
-                src="https://firebasestorage.googleapis.com/v0/b/todo-app-b057b.appspot.com/o/LB%20Finance%20Annual%20Report%20-%202024-new.mp4?alt=media&token=c9428274-cd10-4c9c-aa73-5ff2df926449"
-                type="video/mp4"
-              />
-              <track
-                kind="captions"
-                src="https://firebasestorage.googleapis.com/v0/b/todo-app-b057b.appspot.com/o/LB%20Finance%20Annual%20Report%20-%202024-new.mp4?alt=media&token=c9428274-cd10-4c9c-aa73-5ff2df926449"
-                srcLang="en"
-                label="English captions"
-                default
-              />
-            </>
-          )}
-        Your browser does not support the video tag.
-      </video>
+      />
     </div>
   );
-};
-
-export default HomePageMainVideo;
+}
